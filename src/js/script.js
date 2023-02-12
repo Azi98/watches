8b099750d0b47e51f25c783aa383584eab02793c
Aznaur <bedraev98@gmail.com>
2023-02-12 17:04:17 +0100

    taby 2

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,41 @@ $(document).ready(function(){
             },
           ]
     });
+    
+/*     Код активирует/деактвирует три таба (для фитнекса, для бега, для триатлона) */
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this) /* зис ссылается на тот элемент, на который мы только что нажали */
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+    
+/*     $('.catalog__link').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog__m-content').eq(i).toggleClass('catalog__m-content_active');
+            $('.catalog__list').eq(i).toggleClass('catalog__list_active');
+        })
+    })
+
+    $('.catalog__back').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog__m-content').eq(i).toggleClass('catalog__m-content_active');
+            $('.catalog__list').eq(i).toggleClass('catalog__list_active');
+        })
+    }) Закомментированные выше функции описаны и вызваны более оптимальным способом внизу. Оставил комментарий, чтобы потом понятнее был код */
+
+/*    Код меняет карту (подробнее/назад) */
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog__m-content').eq(i).toggleClass('catalog__m-content_active');
+                $('.catalog__list').eq(i).toggleClass('catalog__list_active');
+            })
+        })
+    }
+
+    toggleSlide('.catalog__link');
+    toggleSlide('.catalog__back');
   });
